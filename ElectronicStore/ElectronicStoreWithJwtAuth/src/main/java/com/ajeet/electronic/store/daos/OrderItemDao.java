6fbec54ef733a5cc0023755ae6458f08e737fd7e
AJEET KUSHWAHA <51816732+ajeet-lab@users.noreply.github.com>
2024-08-17 16:41:57 +0530

    updated

--- ElectronicStore/ElectronicStoreWithJwtAuth/src/main/java/com/ajeet/electronic/store/daos/OrderItemDao.java
+++
@@ -1,7 +0,0 @@
-package com.ajeet.electronic.store.daos;
-
-import com.ajeet.electronic.store.entities.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemDao extends JpaRepository<OrderItem, Integer> {
-}
